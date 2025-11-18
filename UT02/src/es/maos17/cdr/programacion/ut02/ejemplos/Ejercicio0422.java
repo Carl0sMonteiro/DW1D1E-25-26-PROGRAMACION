90b3af7be4c50cd0cd19583f1849096379422c49
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-11-18 19:26:00 +0100

    Create Ejercicio0422.java

---
+++ UT02/src/es/maos17/cdr/programacion/ut02/ejemplos/Ejercicio0422.java
@@ -0,0 +1,11 @@
+package es.maos17.cdr.programacion.ut02.ejemplos;
+
+public class Ejercicio0422 {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i < 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
