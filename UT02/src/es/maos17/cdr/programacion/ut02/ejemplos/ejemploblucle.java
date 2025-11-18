6a315f47b1e261cdb54608329d0cf77504fe8eb3
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-11-18 19:25:42 +0100

    Create ejemploblucle.java

---
+++ UT02/src/es/maos17/cdr/programacion/ut02/ejemplos/ejemploblucle.java
@@ -0,0 +1,11 @@
+package es.maos17.cdr.programacion.ut02.ejemplos;
+
+public class ejemploblucle {
+
+	public static void main(String[] args) {
+		for (int i = 10; i < 20; i++) {
+            System.out.println(i);
+        }
+	}
+
+}
