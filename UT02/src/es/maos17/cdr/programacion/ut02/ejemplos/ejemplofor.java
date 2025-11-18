fd76cd9a284d7b58a645f68091d13936297074de
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-11-18 19:25:49 +0100

    Create ejemplofor.java

---
+++ UT02/src/es/maos17/cdr/programacion/ut02/ejemplos/ejemplofor.java
@@ -0,0 +1,13 @@
+package es.maos17.cdr.programacion.ut02.ejemplos;
+
+public class ejemplofor {
+
+	public static void main(String[] args) {
+	int num;
+	for(num = 0; num < 100;) {
+		System.out.print(num);
+	}
+		
+	}
+
+}
