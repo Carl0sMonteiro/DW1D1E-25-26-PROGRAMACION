e6950f6e8becd80541955a511c4163dd06da5784
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-11-24 17:00:07 +0100

    Create perro.java

---
+++ UT02/src/UT4/ejercicios/perro.java
@@ -0,0 +1,11 @@
+package UT4.ejercicios;
+
+public class perro {
+	private String nombre;
+	private String raza;
+	private String propetario;
+	private String edadanios;
+	private String pesoKG;
+	}
+
+ 
