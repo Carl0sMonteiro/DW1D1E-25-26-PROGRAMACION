e607c7f4f146e11f34edca0abd7c8b5740d9e133
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-12-01 16:54:38 +0100

    Update perro.java

--- UT02/src/UT4/ejercicios/perro.java
+++ UT02/src/UT4/ejercicios/perro.java
@@ -6,6 +6,37 @@ public class perro {
 	private String propetario;
 	private String edadanios;
 	private String pesoKG;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getRaza() {
+		return raza;
+	}
+	public void setRaza(String raza) {
+		this.raza = raza;
+	}
+	public String getPropetario() {
+		return propetario;
+	}
+	public void setPropetario(String propetario) {
+		this.propetario = propetario;
+	}
+	public String getEdadanios() {
+		return edadanios;
+	}
+	public void setEdadanios(String edadanios) {
+		this.edadanios = edadanios;
+	}
+	public String getPesoKG() {
+		return pesoKG;
+	}
+	public void setPesoKG(String pesoKG) {
+		this.pesoKG = pesoKG;
+	}
 	}
 
  
