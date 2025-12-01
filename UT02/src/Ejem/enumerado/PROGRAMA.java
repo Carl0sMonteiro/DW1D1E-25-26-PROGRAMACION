abd2272856397665d2aae87e4e517d6a1706aca9
Carl0sMonteiro <carlosborrero244@gmail.com>
2025-12-01 16:54:26 +0100

    Create PROGRAMA.java

---
+++ UT02/src/Ejem/enumerado/PROGRAMA.java
@@ -0,0 +1,14 @@
+package Ejem.enumerado;
+
+public class PROGRAMA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		dia dia1= dia.LUNES;
+				
+		System.out.println(dia1.name());
+		System.out.println(dia1.toString());
+		System.out.println(dia1);
+	}
+
+}
